64b72ff0daa85e63780cdeed250f3dd8e85f6d69
lkj41110 <807637152@qq.com>
2017-02-20 13:49:38 +0800

    剑指offer题目
    剑指offer题目

---
+++ offer_test/src/Solution2.java
@@ -0,0 +1,15 @@
+/**
+ * 求1+2+3+...+n，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
+ */
+public class Solution2 {
+	public static void main(String[] args) {
+		System.out.println(add(10));
+	}
+	
+	private static int add(int n){
+		int result=n;
+		boolean flag=(n>0)&&((result+=add(n-1))>0);
+		return result;
+	}
+	
+}
