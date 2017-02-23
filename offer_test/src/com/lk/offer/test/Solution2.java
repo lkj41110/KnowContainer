dab12933517435f496134c28110f04e942005e3a
lkj41110 <807637152@qq.com>
2017-02-23 11:57:33 +0800

    剑指Offer 二维数组中的查找
    剑指Offer—— 二维数组中的查找

--- offer_test/src/com/lk/offer/test/Solution2.java
+++ offer_test/src/com/lk/offer/test/Solution2.java
@@ -1,16 +1,31 @@
 package com.lk.offer.test;
+
 /**
- * 求1+2+3+...+n，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
+ * 在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，
+ * 判断数组中是否含有该整数。
  */
 public class Solution2 {
-	public static void main(String[] args) {
-		System.out.println(add(10));
+	public boolean Find1(int target, int[][] array) {
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				if (target == array[i][j])
+					return true;
+			}
+		}
+		return false;
 	}
-	
-	private static int add(int n){
-		int result=n;
-		boolean flag=(n>0)&&((result+=add(n-1))>0);
-		return result;
+
+	public boolean Find2(int target, int[][] array) {
+		int row = 0;
+		int col = array[0].length - 1;
+		while (row <= array.length - 1 && col >= 0) {
+			if (target == array[row][col])
+				return true;
+			else if (target > array[row][col])
+				row++;
+			else
+				col--;
+		}
+		return false;
 	}
-	
 }
