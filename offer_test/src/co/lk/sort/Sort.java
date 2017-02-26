20351c40ef6824c9749b2571eda8e97f919b56c7
lkj41110 <807637152@qq.com>
2017-02-26 14:11:04 +0800

    update sort
    希尔，快速，选择排序完善

--- offer_test/src/co/lk/sort/Sort.java
+++ offer_test/src/co/lk/sort/Sort.java
@@ -1,5 +1,7 @@
 package co.lk.sort;
 
+import java.util.Arrays;
+
 /**
  * 排序,都从小到大
  * 
@@ -10,7 +12,7 @@ public class Sort {
 	/**
 	 * 选择排序
 	 */
-	public static void SelectionSort(int[] arr) {
+	public static void selectionSort(int[] arr) {
 		for (int i = 0; i < arr.length; i++) {
 			int min = arr[i];
 			int k = i;
@@ -33,34 +35,114 @@ public class Sort {
 	/**
 	 * 插入排序
 	 */
-	public static void BubbleSort(int[] arr){
-		for(int i=1;i<arr.length;i++){
-			int k=i-1,temp=arr[i];
-			while(k>=0){
-				if(temp>=arr[k]){
+	public static void bubbleSort(int[] arr) {
+		for (int i = 1; i < arr.length; i++) {
+			int k = i - 1, temp = arr[i];
+			while (k >= 0) {
+				if (temp >= arr[k]) {
 					break;
 				}
-				arr[k+1]=arr[k];
+				arr[k + 1] = arr[k];
 				k--;
 			}
-			arr[k+1]=temp;
+			arr[k + 1] = temp;
 		}
 	}
-	
+
 	/**
 	 * 希尔排序
 	 */
-	public static void ShellSort(int[] arr){
-		
-	}
-	
-	public static void main(String[] args) {
-		int[] arr={-10,0,12,1,100,32,1,-200};
-		BubbleSort(arr);
-		for(int i=0;i<arr.length;i++){
-			System.out.print(arr[i]+" ");
+	public static void shellSort(int[] arr) {
+		int n;// 步长
+		for (n = arr.length / 2; n > 0; n /= 2) {
+			// 插入排序
+			for (int i = n; i < arr.length; i++) {
+				int temp = arr[i];
+				int k = i - n;
+				while (k >= 0 && temp < arr[k]) {
+					arr[k + n] = arr[k];
+					k = k - n;
+				}
+				arr[k + n] = temp;
+			}
 		}
-		
-		
+	}
+
+	/**
+	 * 快速排序
+	 */
+	public static void quicksort(int[] arr) {
+		quicksort0(arr, 0, arr.length - 1);
+	}
+
+	private static void quicksort0(int[] arr, int s, int e) {
+		if (s >= e)
+			return;
+		int temp = arr[s];
+		int i = s, j = e;
+		while (i < j) {
+			while (i < j && arr[j] >= temp) {
+				j--;
+			}
+			if (i < j) {
+				arr[i] = arr[j];
+			}
+			while (i < j && arr[i] <= temp) {
+				i++;
+			}
+			if (i < j) {
+				arr[j] = arr[i];
+			}
+		}
+		arr[i] = temp;
+		quicksort0(arr, s, i - 1);
+		quicksort0(arr, i + 1, e);
+	}
+
+	// 归并排序
+	public static void mergesort(int[] arr) {
+		mergesort0(arr, 0, arr.length-1);
+	}
+
+	private static void mergesort0(int[] arr, int s, int e) {
+		if (s < e) {
+			int mid = (s + e) / 2;
+			mergesort0(arr, s, mid); // 左边有序
+			mergesort0(arr, mid + 1, e); // 右边有序
+			mergearray(arr, s, mid, e); // 再将二个有序数列合并
+		}
+	}
+
+	private static void mergearray(int[] arr, int s, int mid, int e) {
+		// 临时数组
+		int[] arr2 = new int[e - s + 1];
+		int i = s, j = mid + 1;
+		int k = 0;
+		while (i <= mid && j <= e) {
+			if (arr[i] < arr[j]) {
+				arr2[k++] = arr[i++];
+			} else {
+				arr2[k++] = arr[j++];
+			}
+		}
+		while (i <= mid) {
+			arr2[k++] = arr[i++];
+		}
+		while (j <= e) {
+			arr2[k++] = arr[j++];
+		}
+		for (int m = 0; m < arr2.length; m++) {
+			arr[s + m] = arr2[m];
+		}
+	}
+
+	public static void main(String[] args) {
+//		int[] arr = { -10, 0, 12, 1, 100, 32, 1, -200 };
+		int[] arr={3,2,1};
+		mergesort(arr);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+
 	}
 }
